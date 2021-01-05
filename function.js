9f5e05a631c30564c76c92506fdb6963e691c2c8
vincentevoid <vincentevoid@gmail.com>
2021-01-04 19:12:27 -0600

    initial commit

---
+++ function.js
@@ -0,0 +1,21 @@
+$(document).mousemove(function(e){
+    $("#image").css({left:e.pageX, top:e.pageY});
+});
+
+var
+    hoverBox   = $('.box-selector'),
+    hoverImage = $('#image');
+
+hoverBox.mousemove(function(e) {
+    hoverImage.css({
+        left: e.pageX,
+        top:  e.pageY
+    });
+}).mouseout(function() {
+    hoverImage.css({
+        left: -10000,
+        top:  -10000
+    });
+});
+
+// JavaScript Document
