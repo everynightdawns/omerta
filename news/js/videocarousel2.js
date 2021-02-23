7a9b1f77c5af2e71d4fb376fb218898b3047a6fe
vincentevoid <vincentevoid@gmail.com>
2021-02-22 23:03:15 -0600

    Updated news sections

--- news/js/videocarousel2.js
+++ news/js/videocarousel2.js
@@ -1,20 +1,20 @@
 var myVideo=document.getElementById("video2");
 var videoList=['IMG_1116.mov', 'IMG_1110.mov','IMG_1117.mov','IMG_1115.mov','IMG_1109.mov','IMG_1125.mov','IMG_1123.mov','IMG_1131.mov'];
-var index = videoList.indexOf(window.currentVideoName);
+var index2 = videoList.indexOf(window.currentVideoName);
 
 //Next button
 function nextButton2(){
 myVideo.pause();
 myVideo.currentTime=0;
-index = index + 1;
-	if(index==videoList.length){
-		index = 0;
+index2 = index2 + 1;
+	if(index2==videoList.length){
+		index2 = 0;
 	}
-	else if(index==0){
-		index = index + 1;
+	else if(index2==0){
+		index2 = index2 + 1;
 	}
-	myVideo.src = 'img/'+videoList[index];
-	window.currentVideoName=videoList[index];
+	myVideo.src = 'img/'+videoList[index2];
+	window.currentVideoName=videoList[index2];
 	myVideo.play();
 }
 
@@ -22,12 +22,12 @@ index = index + 1;
 function previousButton2(){
 myVideo.pause();
 myVideo.currentTime=0;
-index = index - 1;
-	if(index < 0){
-		index = videoList.length - 1;
+index2 = index2 - 1;
+	if(index2 < 0){
+		index2 = videoList.length - 1;
 	}
-	myVideo.src ='img/'+videoList[index];
-	window.currentVideoName=videoList[index];
+	myVideo.src ='img/'+videoList[index2];
+	window.currentVideoName=videoList[index2];
 	myVideo.play();
 }
 // JavaScript Document
